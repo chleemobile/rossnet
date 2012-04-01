54fe9e50947f490efba4f5af292f1f0f16ece13a
Chayong Lee <chleemobile@gmail.com>
2012-04-01 00:43:14 -0400

    removed _ross

--- trunk/_ross/models/phold/cp_rose_phold.sh
+++
@@ -1,3 +0,0 @@
-mv ./phold.cpp _phold.cpp
-cp ~/dev/backstroke/ROSE/compileTree/projects/backstroke/valueGraph/rose_phold.cpp ./
-mv ./rose_phold.cpp phold.cpp
--- trunk/_ross/models/torus/.svn/text-base/ReadMe.txt.svn-base
+++
@@ -1,2 +0,0 @@
-version 0122 is the working one on CCNI BGL
-version 0123 is added with the outgoing buffer
