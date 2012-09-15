09ae0327c772e048adda8231894e3573e7b185db
Chayong Lee chayong82@gatech.edu <chayong82@mirasol.(none)>
2012-09-14 21:06:01 -0400

    vector worked on mirasol

--- trunk/ross/models/air_traffic/Constants.hpp
+++ trunk/ross/models/air_traffic/Constants.hpp
@@ -1,6 +1,5 @@
-const char *GRAPH_CSV_FILE_PATH = "adjlist.csv";
-//const char *GRAPH_CSV_FILE_PATH = "/nethome/clee333/rossnet-build/ross/models/air_traffic//adjlist.csv";
-
+//const char *GRAPH_CSV_FILE_PATH = "adjlist.csv";
+const char *GRAPH_CSV_FILE_PATH = "/home/chayong82/rossnet-build/ross/models/air_traffic/adjlist.csv";
 
 const int DEBUG = 0;
 
